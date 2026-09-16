7efec9481af676ec0e66db1328544a295ad2f7bd
Iahiax <108660221+Iahiax@users.noreply.github.com>
2026-09-16 15:05:49 +0300

    Rename DEMO to USE_DEMO in config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ EMAIL = "yahia.x@outlook.sa"
 API_KEY = "ut2RpxSbx6fiDdHv"
 API_KEY_PASSWORD = "Yahia@1411"
 PASSWORD = "Yahia-1411"
-DEMO = True
+USE_DEMO = True
 EPIC = "EURUSD"
 RESOLUTION = "MINUTE"
 
