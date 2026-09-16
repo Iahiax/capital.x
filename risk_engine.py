c48f2fa7e21cc9dd644278fadc7a99e08a9b7da7
Iahiax <108660221+Iahiax@users.noreply.github.com>
2026-09-17 02:41:30 +0300

    Sync capital.x risk_engine.py

--- risk_engine.py
+++ risk_engine.py
@@ -1,6 +1,5 @@
-# risk_engine.py
-
 from config import INITIAL_EQUITY
+
 
 class RiskEngine:
     def __init__(self, max_daily_dd=0.03, max_total_dd=0.2):
@@ -10,13 +9,14 @@ class RiskEngine:
         self.total_dd = 0.0
 
     def update(self, equity):
-        dd = (self.equity_start - equity) / self.equity_start
+        if self.equity_start <= 0:
+            self.total_dd = 1.0
+            return
+        dd = max((self.equity_start - equity) / self.equity_start, 0.0)
         self.total_dd = max(self.total_dd, dd)
 
     def can_trade(self, equity_today_start, equity_now):
+        if equity_today_start <= 0:
+            return False
         daily_dd = (equity_today_start - equity_now) / equity_today_start
-        if daily_dd > self.max_daily_dd:
-            return False
-        if self.total_dd > self.max_total_dd:
-            return False
-        return True
+        return daily_dd <= self.max_daily_dd and self.total_dd <= self.max_total_dd
