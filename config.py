b6d2a94475bb0857487999856d234697e8961871
Iahiax <108660221+Iahiax@users.noreply.github.com>
2026-09-16 10:10:07 +0300

    Add configuration settings in config.py

---
+++ config.py
@@ -0,0 +1,12 @@
+# config.py
+
+EMAIL = "yahia.x@outlook.sa"
+API_KEY = "ut2RpxSbx6fiDdHv"
+API_KEY_PASSWORD = "Yahia@1411"
+
+DEMO = True
+EPIC = "EURUSD"
+RESOLUTION = "1M"
+
+FULL_YEAR_MINUTES = 525600
+BATCH_SIZE = 1500
